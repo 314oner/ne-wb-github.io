eb934545b00d4fdb44adcafc314ce5f5d4264106
sasha <schwarzheader@gmail.com>
2026-06-17 05:27:02 -0700

    edit:App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,40 +31,45 @@ const AuthCallbackPage = LazyLoad(lazy(() => import("./views/auth-callback")));
 const ProfilePage = LazyLoad(lazy(() => import("./views/profile")));
 const EditProfilePage = LazyLoad(lazy(() => import("./views/edit-profile")));
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout />,
-    errorElement: ErrorPage,
-    children: [
-      { path: "/", element: HomePage },
-      { path: "/cart", element: CartPage },
-      { path: "/product/:productId", element: ProductPage },
-      { path: "/shops/all", element: ShopsPage },
-      { path: "/shops/:shopId", element: ShopPage },
-      { path: "/auctions/all", element: OpenAuctionsPage },
-      { path: "/auction/:auctionId", element: AuctionPage },
-      { path: "/signin", element: SignInPage },
-      { path: "yandex/callback", element: AuthCallbackPage },
-      { path: "/user/:userId", element: ProfilePage },
-      {
-        element: <PrivateRoute />,
-        children: [
-          { path: "/my-orders", element: MyOrdersPage }, // profile
-          { path: "/order/:orderId", element: OrderPage },
-          { path: "/seller/shops", element: MyShopsPage }, // header
-          { path: "/seller/shop/new", element: CreateShopPage }, // header => element owner check
-          { path: "/seller/shop/:shopId/edit", element: EditShopPage }, // header => element owner check
-          { path: "/seller/:shopId/products/new", element: CreateProductPage },
-          { path: "/my-auctions", element: MyAuctionsPage }, // header
-          { path: "/auction/new", element: CreateAuctionPage }, // header => element owner check
-          { path: "/auction/edit/:auctionId", element: EditAuctionPage }, // header => profile element owner check
-          { path: "/user/:userId/edit", element: EditProfilePage },
-        ],
-      },
-    ],
-  },
-]);
+const basename = import.meta.env.BASE_URL;
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AppLayout />,
+      errorElement: ErrorPage,
+      children: [
+        { path: "/", element: HomePage },
+        { path: "/cart", element: CartPage },
+        { path: "/product/:productId", element: ProductPage },
+        { path: "/shops/all", element: ShopsPage },
+        { path: "/shops/:shopId", element: ShopPage },
+        { path: "/auctions/all", element: OpenAuctionsPage },
+        { path: "/auction/:auctionId", element: AuctionPage },
+        { path: "/signin", element: SignInPage },
+        { path: "yandex/callback", element: AuthCallbackPage },
+        { path: "/user/:userId", element: ProfilePage },
+        {
+          element: <PrivateRoute />,
+          children: [
+            { path: "/my-orders", element: MyOrdersPage }, // profile
+            { path: "/order/:orderId", element: OrderPage },
+            { path: "/seller/shops", element: MyShopsPage }, // header
+            { path: "/seller/shop/new", element: CreateShopPage }, // header => element owner check
+            { path: "/seller/shop/:shopId/edit", element: EditShopPage }, // header => element owner check
+            { path: "/seller/:shopId/products/new", element: CreateProductPage },
+            { path: "/my-auctions", element: MyAuctionsPage }, // header
+            { path: "/auction/new", element: CreateAuctionPage }, // header => element owner check
+            { path: "/auction/edit/:auctionId", element: EditAuctionPage }, // header => profile element owner check
+            { path: "/user/:userId/edit", element: EditProfilePage },
+          ],
+        },
+      ],
+    },
+  ],
+  { basename },
+);
 
 const AppContent = () => <RouterProvider router={router} />;
 

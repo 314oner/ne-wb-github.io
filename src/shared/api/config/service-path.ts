223ac0d56b0448910a76cad0546996c0c0143cb1
sasha <schwarzheader@gmail.com>
2026-06-17 12:56:13 -0700

    debug: env

--- src/shared/api/config/service-path.ts
+++ src/shared/api/config/service-path.ts
@@ -1,2 +1,3 @@
 //src/config/servicePath.ts
-export const API_URL = import.meta.env.DEV ? "" : import.meta.env.VITE_BACKEND_URL || "";
+//export const API_URL = import.meta.env.DEV ? "" : import.meta.env.VITE_BACKEND_URL || "";
+export const API_URL = import.meta.env.DEV ? "" : "https://nest-proto-hub-backend-dev-9.vercel.app";
